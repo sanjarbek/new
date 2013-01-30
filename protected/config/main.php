9720e59b7585afd0ff27a02b13512452ed7d705f
Sanzharbek Amatov <asanjarbek@gmail.com>
2013-01-30 11:34:27 +0600

    Make configuration to connect to db.

--- protected/config/main.php
+++ protected/config/main.php
@@ -47,19 +47,17 @@ return array(
 			),
 		),
 		*/
+		
 		'db'=>array(
-			'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
-		),
-		// uncomment the following to use a MySQL database
-		/*
-		'db'=>array(
-			'connectionString' => 'mysql:host=localhost;dbname=testdrive',
+			'connectionString' => 'mysql:host=localhost;dbname=semamed',
 			'emulatePrepare' => true,
-			'username' => 'root',
-			'password' => '',
+			'username' => 'semamed',
+			'password' => ',trth12',
 			'charset' => 'utf8',
+            'enableProfiling'=>false,
+            'enableParamLogging'=>false,
 		),
-		*/
+        
 		'errorHandler'=>array(
 			// use 'site/error' action to display errors
 			'errorAction'=>'site/error',
